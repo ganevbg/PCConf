c9b01581855f068097637211c5993ad809ef1580
ganevbg <ganevbg@abv.bg>
2021-05-16 21:19:01 +0300

    menu items changed

--- PCConf.Web/Controllers/HomeController.cs
+++ PCConf.Web/Controllers/HomeController.cs
@@ -1,23 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-
-namespace PCConf.Web.Controllers
+﻿namespace PCConf.Web.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
